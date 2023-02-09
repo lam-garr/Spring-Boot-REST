2f3dc7c9837e7e1beb7d192ae00c649664a50cee
Garrett <lamgarr@outlook.com>
2023-02-08 22:08:31 -0800

    Add controller

---
+++ rest/src/main/java/com/example/rest/PersonController.java
@@ -0,0 +1,23 @@
+package com.example.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/persons")
+public class PersonController {
+    
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPersons(){
+        return new ResponseEntity<List<Person>>(personService.getAllPersons(), HttpStatus.OK);
+    }
+}
