c17f6c21f413a18132bc1d9b10846482c8af8c54
Garrett <lamgarr@outlook.com>
2023-02-08 22:09:20 -0800

    Edit annotation for repository

--- rest/src/main/java/com/example/rest/PersonRepository.java
+++ rest/src/main/java/com/example/rest/PersonRepository.java
@@ -2,7 +2,9 @@ package com.example.rest;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends MongoRepository<Person, ObjectId> {
     
 }
