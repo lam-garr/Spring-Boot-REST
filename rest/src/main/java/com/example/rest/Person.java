a9a3e85fa3cd50193eb2912024e9d9c12b7d8e3b
Garrett <lamgarr@outlook.com>
2023-02-08 22:01:03 -0800

    Add model

---
+++ rest/src/main/java/com/example/rest/Person.java
@@ -0,0 +1,26 @@
+package com.example.rest;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    private ObjectId id;
+    private String username;
+    private String password;
+    
+    public Person(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}
