6413a41a589f0e244badedeace8b8214aeef10a9
Garrett <lamgarr@outlook.com>
2023-02-08 22:05:04 -0800

    Add service

---
+++ rest/src/main/java/com/example/rest/PersonService.java
@@ -0,0 +1,17 @@
+package com.example.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    
+    @Autowired
+    private PersonRepository personRepository;
+
+    public List<Person> getAllPersons(){
+        return personRepository.findAll();
+    }
+}
